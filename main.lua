60e4f4c32b470125f95c182d0935bac95f6a82ac
ClaraMackay <clara.mackay@ocsbstudent.ca>
2018-03-06 10:15:31 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,9 @@
+-----------------------------------------------------------------------------------------
+--Title: AnimatingImages
+-- Name: Clara Mackay
+--Course:
+-- displays three images moving in different directions, 
+-- with some coloured text and a background. 
+-----------------------------------------------------------------------------------------
+
+-- Your code here
